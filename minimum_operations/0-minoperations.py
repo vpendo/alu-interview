4abc7c2338db44d53a37376bd0cd950d1ff6dfce
Pendo Vestine <144009226+vpendo@users.noreply.github.com>
2024-05-14 18:14:27 +0200

    Minimum Operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,21 +3,23 @@
 Minimum Operations
 """
 
+
 def minOperations(n):
     """
     Calculates the fewest number of operations needed to result in exactly n H characters in the file.
     """
     if n <= 1:
         return 0
-    
+
     operations = [0] * (n + 1)
     for i in range(2, n + 1):
         if n % i == 0:
             operations[i] = i
             for j in range(i * 2, n + 1, i):
                 operations[j] = min(operations[j], operations[i] + j // i)
-    
+
     return operations[n]
+
 
 if __name__ == "__main__":
     n = 4
@@ -25,3 +27,4 @@ if __name__ == "__main__":
 
     n = 12
     print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
